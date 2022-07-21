b27b2102899873729eaa5f744e17da96ec751b7b
DoudGaya <arbdoule@yahoo.com>
2022-07-21 19:35:03 +0100

    sal

--- codility/who-like-this.js
+++ codility/who-like-this.js
@@ -1,18 +1,18 @@
 
 // solution
 const who = function(names) {
-        switch (names.length) {
-            case 0:
-                return 'no one like this'
-            case 1:
-                return names[0] + ' likes this';
-            case 2:
-                return names[0] + ' and ' + names[1] + ' like this';
-            case 3:
-                return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
-            default:
-                return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this' 
-        }
+    switch (names.length) {
+        case 0:
+            return 'no one like this'
+        case 1:
+            return names[0] + ' likes this';
+        case 2:
+            return names[0] + ' and ' + names[1] + ' like this';
+        case 3:
+            return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
+        default:
+            return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this' 
+    }
 }
 
 
