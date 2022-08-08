c221e763d27006960dd20b31ce230f62fb9e397d
DoudGaya <arbdoule@yahoo.com>
2022-08-08 15:45:37 +0100

    sand

--- codility/domain.js
+++ codility/domain.js
@@ -1,5 +1,14 @@
 
 
 function domainName(url) {
-    return url.replace(/^https?:\/\/|(www\.)?/, '').replace(/\..+$/, '');   
+    return url.replace(/(^https?:\/\/|www.)(www\.)?/, '').replace(/\..+$/, '');  
 }
+
+// test cases for domainName
+console.log(domainName('https://chemcider.com'))
+console.log(domainName('www.demo.com'))
+console.log(domainName('http://foo.co.uk/'))
+console.log(domainName('https://www.chemcider.biz/'))
+console.log(domainName('http://www.chemcider.biz/'))
+console.log(domainName('https://marketplace.visualstudio.com/items?itemName=chrmarti.regex'))
+console.log(domainName('https://github.com/chrmarti/vscode-regex'))
